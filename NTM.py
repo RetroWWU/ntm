f555edea3a48f334f0265f6218f2e427f6b1d657
RetroWWU <105047465+RetroWWU@users.noreply.github.com>
2024-01-04 10:20:45 +0100

    Add files via upload

--- NTM.py
+++ NTM.py
@@ -8,20 +8,20 @@ from NTM_Functions import *
 #--------------------------------------------------------------------------------
 # INPUT
 #--------------------------------------------------------------------------------
+
 parser = argparse.ArgumentParser()
 
 required_args = parser.add_argument_group('required arguments')
-required_args.add_argument("-t","--tree",help="path to newick file or newick tree string", required=True)
-required_args.add_argument("-f","--fasta",help="path to fasta file or fasta string", required=True)
+required_args.add_argument("-t","--tree", help = "path to newick file or newick tree string", required = True)
+required_args.add_argument("-f","--fasta", help = "path to fasta file or fasta string", required = True)
 
-parser.add_argument("-n","--name",help="name of output files")
-parser.add_argument("-d","--dir",help="path to output directory", default="./")
-parser.add_argument("--acro", action="store_true", default=False, help="rename species names to acronyms (e.g. Homo_sapiens -> hom_sap)")
-parser.add_argument("--ctl", action="store_true", default=False, help="create CTL file for paPAML")
-parser.add_argument("--rmd", action="store_true", default=False, help="remove duplicate headers from FASTA")
+parser.add_argument("-n","--name", help = "name of output files")
+parser.add_argument("-d","--dir", help = "path to output directory", default = "./")
+parser.add_argument("--acro", action = "store_true", default = False, help = "rename species names to acronyms (e.g. Homo_sapiens -> hom_sap)")
+parser.add_argument("--ctl", action = "store_true", default = False, help = "create CTL file for paPAML")
+parser.add_argument("--rmd", action = "store_true", default = False, help = "remove duplicate headers from FASTA")
 
 args = parser.parse_args()
-
 tree_input = args.tree
 fasta_input = args.fasta
 name = args.name
@@ -53,7 +53,6 @@ if ">" in fasta_string:
 else:
     fasta_string = reformat_list_headers(fasta_string)
 
-
 #--------------------------------------------------------------------------------
 # SPECIES LIST
 #--------------------------------------------------------------------------------
@@ -62,18 +61,30 @@ if ">" in fasta_string:
     species_list = get_fasta_species(fasta_string)
 else:
     species_list = fasta_string.split("\n")
-    '''v1.1
-    if species_list[-1] == "":
-        species_list = species_list[:-1]
-    '''
-    species_list = [string for string in species_list if string != ""]#v1.2
+    species_list = [string for string in species_list if string != ""] #v1.2
 
+#--------------------------------------------------------------------------------
+# MARK / REMOVE DUPLICATES
+#--------------------------------------------------------------------------------
+
+if len(set(species_list)) < len(species_list):
+    if removeduplicates == False:
+        print("WARNING: There are duplicate FASTA headers. Consider to remove them for paPAML input.")
+
+#--------------------------------------------------------------------------------
+# Adjust species_list to newick tree leaves
+#--------------------------------------------------------------------------------
+
+names = tree.get_leaf_names()
+species_diff = diff(species_list, names)
+species_list = intersection(names, species_list)
+fasta_string = adapt_fasta(fasta_string, species_list, removeduplicates)
 
 #--------------------------------------------------------------------------------
 # PRUNE TREE
 #--------------------------------------------------------------------------------
 
-pruned_tree = prune(bigtree=tree_string,species=species_list)
+pruned_tree = prune(bigtree = tree_string, species = species_list)
 
 #--------------------------------------------------------------------------------
 # ACRO
@@ -83,47 +94,40 @@ acronyms = {}
 for species in species_list:
     acronym = ""
     for i in species.split("_"):
-        acronym += i[0:3]+"_"
+        acronym += i[0:3] + "_"
     acronyms[species] = acronym[:-1].lower()
 
 if acro == True:
-
     for acronym in acronyms:
-        pruned_tree = pruned_tree.replace(acronym,acronyms[acronym])
-        fasta_string = fasta_string.replace(acronym,acronyms[acronym])
-
-#--------------------------------------------------------------------------------
-# MARK / REMOVE DUPLICATES
-#--------------------------------------------------------------------------------
-
-if len(set(species_list)) < len(species_list):
-
-    if removeduplicates == True:
-        fasta_string = remove_duplicates(fasta_string)
-
-    else:
-        print("WARNING: There are duplicate FASTA headers. Consider to remove them for paPAML input.")
+        pruned_tree = pruned_tree.replace(acronym, acronyms[acronym])
+        fasta_string = fasta_string.replace(acronym, acronyms[acronym])
 
 #--------------------------------------------------------------------------------
 # OUTPUT FILES
 #--------------------------------------------------------------------------------
 
 if name == None:
-    output_fasta = "seqfile.fasta" 
+    output_fasta = "seqfile.fasta"
     output_tree = "treefile.tre"
     output_ctl = "paPAML.ctl"
+    output_diff = "species.diff"
 else:
-    output_fasta = name+".fasta" 
-    output_tree = name+".tre"
-    output_ctl = name+".ctl"
+    output_fasta = name + ".fasta"
+    output_tree = name + ".tre"
+    output_ctl = name + ".ctl"
+    output_diff = name + ".diff"
 
 if ctl == True:
-    ctl_text = "seqfile = "+output_fasta+"\ntreefile = "+output_tree
-    with open(dir+output_ctl, "w") as file:
+    ctl_text = "seqfile = "+output_fasta + "\ntreefile = " + output_tree
+    with open(dir + output_ctl, "w") as file:
         file.write(ctl_text)
 
-with open(dir+output_fasta, "w") as file:
+with open(dir + output_fasta, "w") as file:
     file.write(fasta_string)
 
-with open(dir+output_tree, "w") as file:
+with open(dir + output_tree, "w") as file:
     file.write(pruned_tree)
+
+with open(dir + output_diff, "w") as file:
+    file.write(",".join(species_diff))
+    
--- NTM_Functions.py
+++ NTM_Functions.py
@@ -4,99 +4,129 @@ import argparse
 from os import path
 from ete3 import Tree
 
-'''
-read file content or from standard input
-'''
+
+#
+# -----------------------------------------------------------------------------
+# Return the overlap / intersection of two arrays
+# -----------------------------------------------------------------------------
+#
+def intersection(a, b):
+    return [e for e in a if e in b]
+
+
+#
+# -----------------------------------------------------------------------------
+# Return the difference of two arrays
+# -----------------------------------------------------------------------------
+#
+def diff(a, b):
+    return [e for e in a if e not in b]
+
+
+#
+# -----------------------------------------------------------------------------
+# read file content or from standard input
+# -----------------------------------------------------------------------------
+#
 def read_input(input):
     if path.exists(input) == True:
         with open(input) as file:
             string = file.read() 
     else:
         string = input
-
     return string
 
-'''
-reformat FASTA headers
-'''
+
+#
+# -----------------------------------------------------------------------------
+# reformat FASTA headers
+# -----------------------------------------------------------------------------
+#
 def reformat_fasta_headers(fasta_string):
-
     new_fasta = ""
-
     for line in fasta_string.split("\n"):
         if ">" in line:
             new_fasta += (line.split("/")[0]) + "\n"
         else:
             new_fasta += line + "\n"
-        
     return new_fasta
 
-'''
-reformat LIST headers
-'''
+
+#
+# -----------------------------------------------------------------------------
+# reformat LIST headers
+# -----------------------------------------------------------------------------
+#
 def reformat_list_headers(fasta_string):
-
     new_fasta = ""
-
     for line in fasta_string.split("\n"):
         new_fasta += (line.split("/")[0]) + "\n"
-        
     return new_fasta
 
-'''
-extract species names from FASTA file as list
-'''
-'''
-def get_fasta_species(fasta_path,format=0):
-    
-    species_list = []
-    with open(fasta_path,"r") as fasta:
-        for line in fasta:
-            if ">" in line:
-                species_list.append(line[1:-1])
-        
-    return species_list
-'''#removed in v1.2 because duplicated
 
-'''
-extract species names from FASTA string as list
-'''
+#
+# -----------------------------------------------------------------------------
+# extract species names from FASTA string as list
+# -----------------------------------------------------------------------------
+#
 def get_fasta_species(fasta_string):
-    
     species_list = []
     for entry in fasta_string.split("\n"):
         if ">" in entry:
             species_list.append(entry[1:])
-        
     return species_list
 
-'''
-tree with all species is pruned with species list
-package: ete3
-'''
-def prune(bigtree,species):
-    
-    from ete3 import Tree
-    
-    tree = Tree(bigtree)
-    tree.prune(species, preserve_branch_length=True)
-    
-    return str(tree.write(format=9))
 
-'''
-remove sequences with duplicate headers
-'''
-def remove_duplicates(fasta_string):
+#
+# -----------------------------------------------------------------------------
+# tree with all species is pruned with species list
+# -----------------------------------------------------------------------------
+#
+def prune(bigtree, species):
+    tree = Tree(bigtree)
+    tree.prune(species, preserve_branch_length = True)
+    return str(tree.write(format = 9))
+
+
+#
+# -----------------------------------------------------------------------------
+# Adapt fasta to species and maybe duplicates
+# -----------------------------------------------------------------------------
+#
+def adapt_fasta(fasta_string, species, removeduplicates):
     new_fasta = ""
-    headers = []
-    for entry in fasta_string.split("\n"):
-        if ">" in entry and entry not in headers:
+    dups = []
+    print(fasta_string)
+    if ">" in fasta_string:
+        for entry in fasta_string.split("\n"):
+            if len(entry) == 0:
+                continue
+            if entry[0] == ">":
+                spec = entry[1:]
+                if spec not in species:
+                    skip = True
+                elif removeduplicates == True and spec in dups:
+                    skip = True
+                else:
+                    new_fasta += entry + "\n"
+                    dups.append(spec)
+                    skip = False
+            elif skip == False:
+                new_fasta += entry + "\n"
+        if skip == False and len(entry) > 0:
             new_fasta += entry + "\n"
-            headers.append(entry)
-            suppress_entry = False
-        elif ">" in entry and entry in headers:
-            suppress_entry = True
-        elif ">" not in entry and suppress_entry == False:
+        return new_fasta
+    else:
+        for spec in fasta_string.split("\n"):
+            if len(spec) == 0:
+                continue
+            if spec not in species:
+                continue
+            elif removeduplicates == True and spec in dups:
+                continue
+            else:
+                new_fasta += spec + "\n"
+                dups.append(spec)
+        if len(spec) > 0:
             new_fasta += entry + "\n"
-    
-    return new_fasta
+        return new_fasta
